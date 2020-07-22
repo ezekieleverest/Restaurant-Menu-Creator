19fe0507322e04dcb0e329acf957d70ea0ef03ed
Ezekiel <zekejenyart@gmail.com>
2020-07-22 13:34:14 -0500

    Initial Commit

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS restaurant_db;
+CREATE DATABASE restaurant_db;
+
+USE restaurant_db;
+
+CREATE TABLE menu (
+    id INT AUTO_INCREMENT;
+    title VARCHAR (30) NOT NULL;
+    description VARCHAR(300) NOT NULL;
+    price DECIMAL (4, 2);
+    category VARCHAR (20) NOT NULL;
+    PRIMARY KEY (id)
+);
