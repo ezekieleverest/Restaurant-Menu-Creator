a4d51fb1b678d35cabb2f2a1839f1ee2a9f99815
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-23 10:58:52 -0500

    file resolution

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,10 @@ CREATE DATABASE restaurant_db;
 USE restaurant_db;
 
 CREATE TABLE menu (
-    id INT AUTO_INCREMENT;
-    title VARCHAR (30) NOT NULL;
-    description VARCHAR(300) NOT NULL;
-    price DECIMAL (4, 2);
-    category VARCHAR (20) NOT NULL;
+    id INT AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    price DECIMAL (4, 2),
+    category VARCHAR (20) NOT NULL,
     PRIMARY KEY (id)
 );
