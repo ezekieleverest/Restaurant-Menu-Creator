2bee512ae30fc2b74a90b634daa60c4683e76cad
Ezekiel <zekejenyart@gmail.com>
2020-07-23 22:41:35 -0500

    added seeds.js, fixed routes

--- models/admin.js
+++ models/admin.js
@@ -1,6 +1,17 @@
+// User model(may have admin role, may not)
+
 // add item
 // remove item
 // update name
 // update desc
 // update price
 // update status
+
+
+// module.exports = function (sequelize, DataTypes) {
+//     var admin = sequelize.define("admin", {
+//         email: DataTypes.STRING,
+//     });
+
+//     return admin;
+// };
--- models/menu.js
+++ models/menu.js
@@ -1,20 +1,20 @@
-module.exports = function(sequelize, DataTypes) {
-    var menu = sequelize.define("menu", {
-      title: DataTypes.STRING,
-      description: DataTypes.STRING,
-      price: DataTypes.DECIMAL,
-      // category: DataTypes.STRING
+module.exports = function (sequelize, DataTypes) {
+  var menu = sequelize.define("menu", {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+  });
+
+  menu.associate = function (models) {
+    // Associating menu with Posts
+    // When an menu is deleted, also delete any associated Posts
+    menu.belongsTo(models.category, {
+      foreignKey: {
+        allowNull: false
+      }
     });
-  
-    menu.associate = function(models) {
-      // Associating menu with Posts
-      // When an menu is deleted, also delete any associated Posts
-      menu.belongsTo(models.category, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-  
-    return menu;
   };
+
+  return menu;
+
+};
