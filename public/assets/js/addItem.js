9fc4d3f7a69f7558b3804a16a8c31609a2ee0daa
Ezekiel <zekejenyart@gmail.com>
2020-07-27 01:55:49 -0500

    added htmls, began functions for functionality

---
+++ public/assets/js/addItem.js
@@ -0,0 +1,11 @@
+module.exports = function addItem() {
+
+    
+var itemSpot = $("<div>").addClass("card").attr("style", "width: 18rem")
+var itemBox = $("<div>").addClass("card-body")
+var itemTitle = $("<h5>").addClass("card-title")
+var itemPrice = $("<h6>").addClass("card-subtitle mb-2 text-muted")
+var itemDesc = $("<p>").addClass("card-text")
+var itemId = $("<div>").addClass("itemId")
+
+}
---
+++ public/assets/js/menus.js
@@ -0,0 +1,14 @@
+
+module.exports = function renderMenu() {
+
+for (let i = 0; i < array.length; i++) {
+    var itemSpot = $("<div>").addClass("card").attr("style", "width: 18rem")
+    var itemBox = $("<div>").addClass("card-body")
+    var itemTitle = $("<h5>").addClass("card-title")
+    var itemPrice = $("<h6>").addClass("card-subtitle mb-2 text-muted")
+    var itemDesc = $("<p>").addClass("card-text")
+    var itemId = $("<div>").addClass("itemId")
+
+};
+
+}
