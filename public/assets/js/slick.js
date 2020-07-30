276c9707348bb506a8edf33efc9d892eb6714d53
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-29 19:09:06 -0500

    carousel working with slick

---
+++ public/assets/js/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#slideshow .slick").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+    });
+  });
+
+
