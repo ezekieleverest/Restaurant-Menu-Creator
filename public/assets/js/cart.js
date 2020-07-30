f7c228d69d9da519b1143221881c47d5b317e852
Ezekiel <zekejenyart@gmail.com>
2020-07-30 10:00:56 -0500

    Fixed cart

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -1,16 +1,59 @@
-loadEventListeners();
 
-function loadEventListeners(){
-    //when new item is added
-    menus.addEventListener("click", buyItem);
 
-function buyItem(e){
-    if(e.target.classList.contains("add-to-cart")){
+
+const cartItems = JSON.parse(localStorage.getItem('cart')) || []
+const shoppingCartContent = $('#cartItems')
+const clearCartBtn = $('#clear-cart');
+
+$(document).ready(function () {
+    displayCart()
+})
+
+function displayCart() {
+    const html = cartItems.map(x => buildItem(x)).join("")
+    shoppingCartContent.empty().html(html)
+
+    if (cartItems.length) {
+        $("#notification-badge").show().text(cartItems.length)
+    } else {
+        $("#notification-badge").hide()
+    }
+}
+
+function buildItem(menu) {
+    return `
+<tr class="text-light">
+    <td>
+        ${menu.title}
+    </td>
+     <td>
+        ${menu.price}
+    </td>
+     <td>
+        <button onclick="removeItem(event)" data-itemId="${menu.id}">X</button>
+    </td>
+</tr>
+`;
+}
+
+function removeItem(e) {
+    const _id = parseInt($(e.target).attr('data-itemId'))
+    const _index = cartItems.findIndex(x => x.id === _id)
+
+    cartItems.splice(_index, 1)
+    localStorage.setItem('cart', JSON.stringify(cartItems))
+
+    displayCart()
+}
+
+function buyItem(e) {
+    if (e.target.classList.contains("add-to-cart")) {
         //read the menu value
-     const menu = e.target.parentElement.parentElement;
+        const menu = e.target.parentElement.parentElement;
         getMenuInfo(menu);
     }
-}}
+}
+
 
 function getMenuInfo(menu) {
     //create an Object with menu data
@@ -22,46 +65,14 @@ function getMenuInfo(menu) {
     }
     addToCart(menuInfo);
 }
-function addToCart(menuInfo){
-    const row = document.createElement('#cart-target');
 
-    row.innerHTML = `
-<tr>
-    <td>
-        ${menu.title}
-    </td>
-     <td>
-        ${menu.price}
-    </td>
-     <td>
-        <a href="#" class="remove" data-id="${menu.id}">X</a>
-    </td>
-</tr>
-`
-    ;
 
-    shoppingCartContent.appendChild(row);
-}
 
-shoppingCartContent = document.querySelector('#cart-content tbody'),
-clearCartBtn = document.querySelector('#clear-cart');
-loadEventListeners();
 
-function loadEventListeners(){
-    //when new item is added
-    menus.addEventListener('click', buyItem);
-    shoppingCartContent.addEventListener('click', removeItem);
-    clearCartBtn.addEventListener('click', clearCart);
-}
-function removeItem(e){
-    if(e.target.classList.contains('remove')){
-         e.target.parentElement.parentElement.remove();
-    }
-}
 
-function clearCart(){
+function clearCart() {
     console.log(shoppingCartContent.firstChild)
-    while(shoppingCartContent.firstChild){
+    while (shoppingCartContent.firstChild) {
         shoppingCartContent.removeChild(shoppingCartContent.firstChild);
     }
 }
